e5a5039b2e8be12b252ee2d882b6ac79a7b9ddb3
MirellaLimaCruz <limacruz2603@gmail.com>
2025-05-27 15:47:01 -0300

    Add files via upload

--- TDE2p2.py
+++ TDE2p2.py
@@ -125,4 +125,16 @@ else:
     logaritmo = math.log(n)
     print (f"{logaritmo:.2f}")
 
-#13. Faça um algoritmo que calcule a méia ponderada das notas de 3 provas. A primeira e a segunda prova tem peso 1 e a terceira tem peso 2. Ao final, mostrar a média do aluno e indicar se o aluno foi aprovado ou reprovado. A nota para aprovação deve ser igual ao superior a 60 pontos.
+#13. Faça um algoritmo que calcule a média ponderada das notas de 3 provas. A primeira e a segunda prova tem peso 1 e a terceira tem peso 2. Ao final, mostrar a média do aluno e indicar se o aluno foi aprovado ou reprovado. A nota para aprovação deve ser igual ao superior a 60 pontos.
+print("Digite três notas para obter a média ponderada delas.")
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+n3 = float(input("Nota 3: "))
+
+media_ponderada = ((n1 * 1) + (n2 * 1) + (n3 * 2))/4
+if media_ponderada < 60:
+    print("Reprovado!")
+else:
+    print("Aprovado!")
+
+#14. A nota final de um estudante é calculada a partir de três notas atribuídas entre o intervalo de 0 até 10, respe
